d22181d5c2755bdf748650dc4a3ba01394386e84
lucas <lucas.videcoq1@gmail.com>
2021-03-08 16:22:08 +0100

    lorem ipsum

--- index.php
+++ index.php
@@ -5,13 +5,27 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
     <title>Home</title>
 </head>
 <body>
+
+    <section id="nav">
+        <div class="navbar">
+            <ul id="navbar">
+                <li><a></a>Accueil</li>
+                <li><a></a>Projets</li>
+                <li><a></a>Contact</li>
+                <li><a>A propos</a></li>
+            </ul>
+        </div>
+    </section>
+
+    <section id=""
     <h1>Test page</h1>
 
     <?php
-        echo "PHP work correctly YAY!"
+        echo "PHP work correctly YAY!";
     ?>
 </body>
 </html>
